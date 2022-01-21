2f7f0d232957868e8a0909d65c3cd82f84664147
bhaskar_kumar <70895654+bhaskarkumarpro@users.noreply.github.com>
2022-01-21 08:07:23 +0530

    Update Problem2.java

--- Day1/src/Problem2.java
+++ Day1/src/Problem2.java
@@ -3,7 +3,22 @@ public class Problem2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("bhaskar");
+		System.out.println("Exploring Label");
+		outer:for(int i=1;i<=5;i++)  
+			  {  
+				System.out.println();  
+				for(int j=1;j<=5;j++)
+				{
+					if((i+j)%5 == 0)
+						break outer;
+					else {
+						System.out.println(i+j);
+					}
+					System.out.println("not reached till  th multiple of 5 ");
+				}
+			  }
+				
+		
 	}
 
 }
